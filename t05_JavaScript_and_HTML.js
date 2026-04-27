9023bd4d731a66ef38dcd6c782d035a6f55b8d3c
25479ns <25479ns@hvhs.school.nz>
2026-04-27 22:31:11 +0000

    Apr 27, 2026, 10:31 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,48 @@
-/* Paste your code from task 3, Maths here */
+/****************************
+This is a block comment.
+Put your header comment here!
+****************************/
+
+console.log("Running t02_variables.js")
+console.log("Working with variables in Javascript")
+
+// Variables
+
+let name = "Sandro";
+console.log(name);
+
+let age = 15;
+console.log(age);
+
+let hobbies = ["Coding", "Reading", "Cooking"];
+console.log(hobbies);
+
+let clothes = ["T-shirt", "Jeans", "Hoddie", "Shoes"];
+console.log(clothes);
+
+let isFlooded = true;
+console.log(isFlooded);
+
+
+var number1 = 8;
+var number2 = 4;
+var answer;
+
+answer = number1 + number2;
+console.log("add: " + answer);
+answer = number1 - number2;
+console.log("subtract: " + answer);
+answer = number1 * number2;
+console.log("multiply: " + answer);
+answer = number1 / number2;
+console.log("divide: " + answer);
+
+/***************************
+Main Code
+****************************/
+OUTPUT.innerhtml += "<h2>Added by JavaScript</h2>";
+OUTPUT.innerhtml += "<p></p>";
+
+/***************************
+Functions
+****************************/
