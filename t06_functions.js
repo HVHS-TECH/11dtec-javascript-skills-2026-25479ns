39a90dac1063c48b29990b8799f503aba3a2d5ed
25479ns <25479ns@hvhs.school.nz>
2026-04-27 22:54:10 +0000

    Apr 27, 2026, 10:54 PM

--- t06_functions.js
+++ t06_functions.js
@@ -1,1 +1,51 @@
-/* Paste your code from task 5 here */
+/****************************
+This is a block comment.
+Put your header comment here!
+****************************/
+
+console.log("Running t02_variables.js")
+console.log("Working with variables in Javascript")
+
+// Variables
+
+let name = "Sandro";
+console.log(name);
+
+let age = 15;
+console.log(age);
+
+let hobbies = ["Coding", "Reading", "Cooking"];
+console.log(hobbies);
+
+let clothes = ["T-shirt", "Jeans", "Hoddie", "Shoes"];
+console.log(clothes);
+
+let isFlooded = true;
+console.log(isFlooded);
+
+
+var number1 = 8;
+var number2 = 4;
+var answer;
+
+answer = number1 + number2;
+console.log("add: " + answer);
+answer = number1 - number2;
+console.log("subtract: " + answer);
+answer = number1 * number2;
+console.log("multiply: " + answer);
+answer = number1 / number2;
+console.log("divide: " + answer);
+
+/***************************
+Main Code
+****************************/
+const OUTPUT = document.getElementById("javaScriptOutput");
+OUTPUT.innerHTML += "<p>Added by JavaScript</p>";
+OUTPUT.innerHTML += "<p>Hello, Mr. Sandro!</p>";
+OUTPUT.innerHTML += "<p>Today is a good day to learn JavaScript</p>";
+OUTPUT.innerHTML += "<p>Best of luck with your 1.2 English assessment but don't worry, you've got this</p>";
+
+/***************************
+Functions
+****************************/
