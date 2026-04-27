ad5efeff1acff88d2ca8ed0e71122a9f757b82c4
25479ns <25479ns@hvhs.school.nz>
2026-04-27 22:53:21 +0000

    Apr 27, 2026, 10:53 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -44,7 +44,7 @@ const OUTPUT = document.getElementById("javaScriptOutput");
 OUTPUT.innerHTML += "<p>Added by JavaScript</p>";
 OUTPUT.innerHTML += "<p>Hello, Mr. Sandro!</p>";
 OUTPUT.innerHTML += "<p>Today is a good day to learn JavaScript</p>";
-OUTPUT.innerHTML += "<p>Best of luck with your 1.2 English assessment but don't worry, you've got this, as you will receive seven spells</p>";
+OUTPUT.innerHTML += "<p>Best of luck with your 1.2 English assessment but don't worry, you've got this</p>";
 
 /***************************
 Functions
