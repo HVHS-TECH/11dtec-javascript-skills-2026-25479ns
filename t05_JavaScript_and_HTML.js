64dc5ee60fe7e110c996da16233e59f46391feee
25479ns <25479ns@hvhs.school.nz>
2026-04-27 22:43:49 +0000

    Apr 27, 2026, 10:43 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -40,6 +40,7 @@ console.log("divide: " + answer);
 /***************************
 Main Code
 ****************************/
+const OUTPUT = document.getElementById("javaScriptOutput");
 OUTPUT.innerHTML += "<h2>Added by JavaScript</h2>";
 OUTPUT.innerHTML += "<p>Hello, Mr. Sandro!</p>";
 OUTPUT.innerHTML += "<p>Today is a good day to learn JavaScript</p>";
