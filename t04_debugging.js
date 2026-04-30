4f1b0206b32de9a5afb0d56869a95a502b08fcf0
25479ns <25479ns@hvhs.school.nz>
2026-04-30 03:04:11 +0000

    Apr 30, 2026, 3:04 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -1,20 +1,24 @@
-console.log("Task 4 - Debugging");
+console.log("Task 4 - Debugging")
 
-// 1. Set up variables first so they are defined for calculations
+// calculate the area
+var area = length * width;
+
+// calculate the perimeter
+let perimeter = 2 * length + 2 * width;
+
+// Set up the size of the page.
 var length = 8;
 var width = 10;
 
-// 2. Calculate using defined variables
-var area = length * width; 
-var perimeter = 2 * (length + width); 
+// Display the results
+console.log("A area of a piece of paper with a length of "+length+", and a width of "+width+" is "+ area)
+console.log("The same piece of paper has a perimeter of "+perimeter)
 
-// 3. Display results with correct string concatenation
-console.log("An area of a piece of paper with a length of " + length + " and a width of " + width + " is " + area);
-console.log("The same piece of paper has a perimeter of " + perimeter);
-
-// 4. Check logic: for these dimensions, area (80) should be larger than perimeter (36)
-if (area > perimeter) {
-    console.log("Yay, your area is larger, this is right for this example");
-} else {
+// Check, does this result look sensible
+if (perimeter > area)
+{
+console.log("Yay, your area is larger, this is right for this example");
+}
+else{
     console.log("Hmm, your perimeter is larger. It shouldn't be for this example");
 }
