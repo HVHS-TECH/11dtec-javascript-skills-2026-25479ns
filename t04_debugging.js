e66273be43397feb3204f9f892836a6b717fa3b2
25479ns <25479ns@hvhs.school.nz>
2026-04-30 03:08:31 +0000

    Apr 30, 2026, 3:08 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -14,9 +14,8 @@ console.log("The same piece of paper has a perimeter of "+perimeter)
 
 // Check, does this result look sensible
 if (perimeter > area)
-{
+
 console.log("Yay, your area is larger, this is right for this example");
-}
-else{
-    console.log("Hmm, your perimeter is larger. It shouldn't be for this example");
-}
+
+console.log("Hmm, your perimeter is larger. It shouldn't be for this example");
+
