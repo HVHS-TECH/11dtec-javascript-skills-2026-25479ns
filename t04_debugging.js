c4e9ff8c49608c7997af87d231e9aa34907f2fee
25479ns <25479ns@hvhs.school.nz>
2026-05-03 21:14:03 +0000

    May 3, 2026, 9:14 PM

--- t04_debugging.js
+++ t04_debugging.js
@@ -1,22 +1,19 @@
-console.log("Task 4 - Debugging")
+console.log("Task 4 - Debugging");
 
-// calculate the area
-var area = length * width;
-
-// calculate the perimeter
-let perimeter = 2 * length + 2 * width;
-console.log("A area of a piece of paper with a length of "+length+", and a width of "+width+" is "+ area)
-
-// Set up the size of the page.
+// 1. Move declarations to the top
 var length = 8;
 var width = 10;
-console.log("The same piece of paper has a perimeter of "+perimeter)
 
-// Check, does this result look sensible
-if (perimeter > area)
-{
-console.log("Yay, your area is larger, this is right for this example");
-}
-else{
-    console.log("Hmm, your perimeter is larger. It shouldn't be for this example");
+// 2. Now perform the calculations
+var area = length * width;
+let perimeter = 2 * length + 2 * width;
+
+console.log("An area of a piece of paper with a length of " + length + ", and a width of " + width + " is " + area);
+console.log("The same piece of paper has a perimeter of " + perimeter);
+
+// Check results
+if (area > perimeter) { 
+    console.log("Yay, your area is larger, this is right for this example");
+} else {
+    console.log("Hmm, your perimeter is larger.");
 }
