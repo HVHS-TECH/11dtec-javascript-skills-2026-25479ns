59827d6f1b643148dd57bbce54f4ebf34296bec9
25479ns <25479ns@hvhs.school.nz>
2026-05-04 22:20:40 +0000

    May 4, 2026, 10:20 PM

--- t08_scope.js
+++ t08_scope.js
@@ -3,6 +3,16 @@ const output = document.getElementById("output");
 var Myvar = 0;
 output.innerHTML += "Start: " + Myvar + "<br>";
 
-function
+functionOne();
+function functionOne(){
+    Myvar = 1;
+    output.innerHTML += "Function One: " + Myvar + "<br>";
+}
 
+functionTwo();
+function functionTwo(){
+    Myvar = 2;
+    output.innerHTML += "Function Two: " + Myvar + "<br>";
+}
 
+output.innerHTML += "End: " + Myvar + "<br>";
