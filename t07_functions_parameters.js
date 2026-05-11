ad4cf2a8776532d7c31481c710ef9e78c12543e4
25479ns <25479ns@hvhs.school.nz>
2026-05-11 22:23:25 +0000

    May 11, 2026, 10:23 PM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -49,8 +49,42 @@ OUTPUT.innerHTML += "<p>Best of luck with your 1.2 English assessment but don't 
 /***************************
 Functions
 ****************************/
-function displayWelcome(_name, _age){
-    OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"!</p>";
-    OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
+welcome();
+displayProduct("Chocolate bar", 4);
+displayProduct("chip", 3);
+displayProduct("drink", "2.50");
+OUTPUT.innerHTML += "<br>";
+OUTPUT.innerHTML += "<br>";
 
-}
+//Adding it to the doc
+OUTPUT.innerHTML += "<p>Hello " + userName + "</p>";
+OUTPUT.innerHTML += "<p>As of " +currentYear + " you are " + userAge + " years old</p>";
+tmp = currentYear - userAge;
+OUTPUT.innerHTML += "<p>You were born in " + tmp + "</p>";
+tmp = userAge+10;
+OUTPUT.innerHTML += "<p>In 10 years you will be " + tmp + " years old</p>";
+OUTPUT.innerHTML += "<p>You have $" + userMoney + "</p>";
+tmp = userMoney/2;
+OUTPUT.innerHTML += "<p>You spend half of your money, now you have $" + tmp + "</p>";
+tmp = (userMoney/2)+3;
+OUTPUT.innerHTML += "<p>Then you get 3 dollars. now you have $" + tmp + "</p>";
+
+//what it's supposed to say
+/*
+Hi _______
+
+As of ____ you are __ years old
+
+You were born in ____
+
+In 10 years you will be __ years old
+
+You have __ dollars
+
+You spend half of your money, now you have __
+
+Then you get $3, now you have __
+*/
+
+
+console.log("code complete");
