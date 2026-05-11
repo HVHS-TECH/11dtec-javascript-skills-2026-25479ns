29c0a74933d4dd137e3cc154e2ae73a7834ad871
25479ns <25479ns@hvhs.school.nz>
2026-05-11 22:27:01 +0000

    May 11, 2026, 10:27 PM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -51,7 +51,7 @@ OUTPUT.innerHTML += "<p>Best of luck with your 1.2 English assessment but don't 
 /***************************
 Functions
 ****************************/
-welcome();
+function displayProduct(name, price){
 displayProduct("Chocolate bar", 4);
 displayProduct("chip", 3);
 displayProduct("drink", "2.50");
@@ -70,23 +70,5 @@ tmp = userMoney/2;
 OUTPUT.innerHTML += "<p>You spend half of your money, now you have $" + tmp + "</p>";
 tmp = (userMoney/2)+3;
 OUTPUT.innerHTML += "<p>Then you get 3 dollars. now you have $" + tmp + "</p>";
-
-//what it's supposed to say
-/*
-Hi _______
-
-As of ____ you are __ years old
-
-You were born in ____
-
-In 10 years you will be __ years old
-
-You have __ dollars
-
-You spend half of your money, now you have __
-
-Then you get $3, now you have __
-*/
-
 
 console.log("code complete");
