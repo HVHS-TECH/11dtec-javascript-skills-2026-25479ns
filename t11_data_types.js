8a72eaaa2e13345966b004bb2c43ed0c5d3f6dca
25479ns <25479ns@hvhs.school.nz>
2026-05-25 22:21:27 +0000

    May 25, 2026, 10:21 PM

--- t11_data_types.js
+++ t11_data_types.js
@@ -11,7 +11,7 @@ console.log("Working with data types in Javascript")
 let name = "Sandro";
 console.log(name);
 
-let age = 15;
+let age = "15";
 console.log(age);
 
 let hobbies = ["Coding", "Reading", "Cooking"];
