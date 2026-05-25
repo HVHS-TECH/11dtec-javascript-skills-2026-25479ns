d7b05d9bd8a6818a08a3faaa22c30b6aac2639c9
25479ns <25479ns@hvhs.school.nz>
2026-05-25 22:41:19 +0000

    May 25, 2026, 10:41 PM

--- t11_data_types.js
+++ t11_data_types.js
@@ -85,12 +85,12 @@ function start(){
 }
 
 function getFormInput(OUTPUT){
-    const NAME_FIELD = document.getElementById("UserName");
-    const AGE_FIELD = document.getElementById("UserAge");
-    const MONEY_FIELD = document.getElementById("UserMoney");
+    const NAME_FIELD = document.getElementById("NAME_FIELD");
+    const AGE_FIELD = document.getElementById("AGE_FIELD");
+    const MONEY_FIELD = document.getElementById("MONEY_FIELD");
 
-    OUTPUT.innerHTML += "<p>Welcome to my page, " + UserName.value + "</p>";
-    OUTPUT.innerHTML += "<p>You are " + UserAge.value + " years old.</p>";
-    OUTPUT.innerHTML += "<p>You have $" + UserMoney.value + ".</p>";
+    OUTPUT.innerHTML += "<p>Welcome to my page, " + NAME_FIELD.value + "</p>";
+    OUTPUT.innerHTML += "<p>You are " + AGE_FIELD.value + " years old.</p>";
+    OUTPUT.innerHTML += "<p>You have $" + MONEY_FIELD.value + ".</p>";
 }
 
