f56cb5af54c345ad9a214cf7fbc3a768be74cd7b
25479ns <25479ns@hvhs.school.nz>
2026-05-27 01:04:03 +0000

    May 27, 2026, 1:04 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,114 +1,107 @@
-/****************************
-This is a block comment.
-Put your header comment here!
-****************************/
-
-console.log("Running t10_input_from_HTML.js")
-console.log("Working with input from HTML in Javascript")
-
-// Variables
-
-let name = "Sandro";
-console.log(name);
-
-let age = "15";
-console.log(age);
-
-let $$$ = 5;
-console.log($$$);
-
-let hobbies = ["Coding", "Reading", "Cooking"];
-console.log(hobbies);
-
-let clothes = ["T-shirt", "Jeans", "Hoddie", "Shoes"];
-console.log(clothes);
-
-let isFlooded = true;
-console.log(isFlooded);
-
-var number1 = 8;
-var number2 = 4;
-var answer;
+/*------------------------------------------------------------PROGRAM  START------------------------------------------------------------*/
 
 
+console.log("Running t12_conditionals.js");
 
-answer = number1 + number2;
-console.log("add: " + answer);
-answer = number1 - number2;
-console.log("subtract: " + answer);
-answer = number1 * number2;
-console.log("multiply: " + answer);
-answer = number1 / number2;
-console.log("divide: " + answer);
 
-/***************************
-Main Code
-****************************/
-const OUTPUT = document.getElementById("OUTPUT");
-OUTPUT.innerHTML += "<p>Added by JavaScript</p>";
-OUTPUT.innerHTML += "<p>Hello, Mr. Sandro!</p>";
-OUTPUT.innerHTML += "<p>Today is a good day to learn JavaScript</p>";
-OUTPUT.innerHTML += "<p>Best of luck with your 1.2 English assessment but don't worry, you've got this</p>";
-writeline();
+/*---------------------------------------------------------VARIABLE DECLERATION---------------------------------------------------------*/
 
-/***************************
-Functions
-****************************/
-function writeline(){
-    //Add a line to the html page
-    OUTPUT.innerHTML += "<p>Welcome to the page, Sandro!</p>";
-    OUTPUT.innerHTML += "<p>You are 15 years old!</p>";
+
+let userName = ".... ??? hello who? how can you see this? how did you bypass the form? get out of here right now!!!";
+let userAge = 10000000000000000000000000000;
+let userMoney = 0.000000000000000000000000001;
+let currentYear = 1900 + Math.round(Math.random()*199);
+
+let tmp;
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+const NAME_FIELD = document.getElementById("nameField");
+const AGE_FIELD = document.getElementById("ageField");
+const MONEY_FIELD = document.getElementById("moneyField");
+
+
+console.log("variables delcared successfully");
+
+
+/*--------------------------------------------------------------MAIN  CODE--------------------------------------------------------------*/
+
+//I'm going to put the begin function in the main code because  it kind of is the main code. This is like the web version of the "loop"
+//function which should be put in the "MAIN CODE" secition.
+function begin() {
+    userName = NAME_FIELD.value;
+    userAge = Number(AGE_FIELD.value);
+    userMoney = Number(MONEY_FIELD.value);
+    run();
 }
 
-function displayWelcome(name, age){
-//Display a welcome message to the user
-OUTPUT.innerHTML += "<p>Welcome to the page, "+name+"! </p>";
-OUTPUT.innerHTML += "<p>You are "+age+" years old</p>";
+function run() {
+    OUTPUT.innerHTML = "<br>";
+    OUTPUT.innerHTML += "<br>";
+    welcome();
+    OUTPUT.innerHTML += "<p>You have $" + userMoney + "</p>";
+    displayProduct("Chocolate bar", 4, userMoney);
+    displayProduct("chip", 3, userMoney);
+    displayProduct("drink", 2.50, userMoney);
+    OUTPUT.innerHTML += "<br>";
+    OUTPUT.innerHTML += "<br>";
+
+    //Adding it to the doc
+    OUTPUT.innerHTML += "<p>Hello " + userName + "</p>";
+    OUTPUT.innerHTML += "<p>As of " +currentYear + " you are " + userAge + " years old</p>";
+    tmp = currentYear - userAge;
+    OUTPUT.innerHTML += "<p>You were born in " + tmp + "</p>";
+    tmp = userAge+10;
+    OUTPUT.innerHTML += "<p>In 10 years you will be " + tmp + " years old</p>";
+    OUTPUT.innerHTML += "<p>You have $" + userMoney + "</p>";
+    tmp = userMoney/2;
+    OUTPUT.innerHTML += "<p>You spend half of your money, now you have $" + tmp + "</p>";
+    tmp = (userMoney/2)+3;
+    OUTPUT.innerHTML += "<p>Then you get 3 dollars. now you have $" + tmp + "</p>";
+    console.log("function called successfully");
+    OUTPUT.innerHTML += "<p>All this money stuff is merely theoretical. Please go spened all your money on our products above :3</p>";
 }
 
-var myVar = 0;
-OUTPUT.innerHTML += "Start: " + myVar + "<br>";
+//what it's supposed to say
+/*
+Hi _______
 
-functionOne();
-function functionOne(){
-myVar = 1;
-OUTPUT.innerHTML += "functionOne: " + myVar + "<br>";
+As of ____ you are __ years old
+
+You were born in ____
+
+In 10 years you will be __ years old
+
+You have __ dollars
+
+You spend half of your money, now you have __
+
+Then you get $3, now you have __
+*/
+
+
+console.log("code complete");
+
+
+/*---------------------------------------------------------------FUNCTION---------------------------------------------------------------*/
+
+function welcome() {
+    OUTPUT.innerHTML += "<h2>Welcome to the shop</h2>";
+    console.log("function called successfully");
 }
 
-functionTwo();
-function functionTwo(){
-myVar = 2;
-OUTPUT.innerHTML += "Function Two: " + myVar + "<br>";
+function displayProduct(_name, _price, _currentMoney) {
+    OUTPUT.innerHTML += "<p>We are selling " + _name + "s for $" + _price + "</p>";
+    if (_currentMoney >= _price)
+    {
+        OUTPUT.innerHTML += "<p>You can afford this item!</p>";
+        OUTPUT.innerHTML += "<br>";
+    }
+    else
+    {
+        OUTPUT.innerHTML += "<p>Your broke ass can't even aford our cheap ahh " + _name + "s</p>";
+        OUTPUT.innerHTML += "<br>";
+    }
+    console.log("function called successfully using paramiters "+_name+", "+_price+", "+_currentMoney);
 }
 
-OUTPUT.innerHTML += "End: " + myVar + "<br>";
-
-function start(){
-    console.log("Caution Button was clicked!");
-    OUTPUT.innerHTML += "<p>Caution Button was clicked!</p>";
-}
-
-function getFormInput(){
-    const NAME_FIELD = document.getElementById("nameField");
-    const AGE_FIELD = document.getElementById("ageField");
-    const MONEY_FIELD = document.getElementById("moneyField");
-    
-    let userName = NAME_FIELD.value;
-    let userAge = AGE_FIELD.value;
-    let moneyField = MONEY_FIELD.value;
-    
-    OUTPUT.innerHTML += "<p>Welcome to my page, " + name + "</p>";
-    OUTPUT.innerHTML += "<p>You are " + age + " years old.</p>";
-}   OUTPUT.innerHTML += "<p>You have $" +$$$ + " in your bank account.</p>";
-
-if (age < 18){
-    console.log("You are under 18 years old. Get Out!");
-}
-
-if (age >= 18){
-    console.log("You are 18 years old or older. Welcome!");
-}
-
-if (age == 18){
-    console.log("You are exactly 18 years old. Go get the full driver's license!");
-}
+/*------------------------------------------------------------END OF PROGRAM------------------------------------------------------------*/
