873cb4721cc175921361fe71056fa41739a08283
25479ns <25479ns@hvhs.school.nz>
2026-05-27 01:51:03 +0000

    May 27, 2026, 1:51 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,54 +1,97 @@
 /****************************
-Intro:/
+This is a block comment.
+Put your header comment here!
 ****************************/
 
-console.log("Running t12_conditionals.js");
+console.log("Running t10_input_from_HTML.js")
+console.log("Working with input from HTML in Javascript")
 
-// transfer from JS to HTML
+// Variables
+
+let name = "Sandro";
+console.log(name);
+
+let age = "15";
+console.log(age);
+
+let $$$ = 5;
+console.log($$$);
+
+let hobbies = ["Coding", "Reading", "Cooking"];
+console.log(hobbies);
+
+let clothes = ["T-shirt", "Jeans", "Hoddie", "Shoes"];
+console.log(clothes);
+
+let isFlooded = true;
+console.log(isFlooded);
+
+
+var number1 = 8;
+var number2 = 4;
+var answer;
+
+answer = number1 + number2;
+console.log("add: " + answer);
+answer = number1 - number2;
+console.log("subtract: " + answer);
+answer = number1 * number2;
+console.log("multiply: " + answer);
+answer = number1 / number2;
+console.log("divide: " + answer);
+
+/***************************
+Main Code
+****************************/
 const OUTPUT = document.getElementById("OUTPUT");
-OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>"
+OUTPUT.innerHTML += "<p>Added by JavaScript</p>";
+OUTPUT.innerHTML += "<p>Hello, Mr. Sandro!</p>";
+OUTPUT.innerHTML += "<p>Today is a good day to learn JavaScript</p>";
+OUTPUT.innerHTML += "<p>Best of luck with your 1.2 English assessment but don't worry, you've got this</p>";
+writeline();
 
-
-//Functions//Functions
-function welcome(){
-    OUTPUT.innerHTML += "<h1>Welcome to the shop!</h1>"
+/***************************
+Functions
+****************************/
+function writeline(){
+    //Add a line to the html page
+    OUTPUT.innerHTML += "<p>Welcome to the page, Sandro!</p>";
+    OUTPUT.innerHTML += "<p>You are 15 years old!</p>";
 }
-function displayProduct(_name, _price){
-    OUTPUT.innerHTML += "<h2>" + _name + ": $" + _price + "</h2>";
+
+function displayWelcome(name, age){
+//Display a welcome message to the user
+OUTPUT.innerHTML += "<p>Welcome to the page, "+name+"! </p>";
+OUTPUT.innerHTML += "<p>You are "+age+" years old</p>";
 }
-function getUserName(){
-    const NAME_FIELD = document.getElementById("nameField");
-    let userName = NAME_FIELD.value;
-    OUTPUT.innerHTML += "<p>Your name is " + userName + ".</p>";
-  }
-function getUserAge(){
-    const AGE_FIELD = document.getElementById("ageField");
-    let userAge = Number(AGE_FIELD.value);
-    OUTPUT.innerHTML += "<p>You are " + userAge + " years old.</p>";
-  }
-  function getUserMoney(){
-    const MONEY_FIELD = document.getElementById("moneyField");
-    let userMoney = Number(MONEY_FIELD.value);
-    OUTPUT.innerHTML += "<p>You have $" + userMoney + ".</p>";
-if (userMoney >= 4){
-    OUTPUT.innerHTML += "<p>A chocolate bar costs $4, so you can afford a chocolate bar.</p>";
-} else {
-    OUTPUT.innerHTML += "<p>A chocolate bar costs $4, so you can't afford a chocolate bar.</p>";
+
+var myVar = 0;
+OUTPUT.innerHTML += "Start: " + myVar + "<br>";
+
+functionOne();
+function functionOne(){
+myVar = 1;
+OUTPUT.innerHTML += "functionOne: " + myVar + "<br>";
 }
-  }
 
+functionTwo();
+function functionTwo(){
+myVar = 2;
+OUTPUT.innerHTML += "Function Two: " + myVar + "<br>";
+}
 
-// This code adds a message to the page
-
-
+OUTPUT.innerHTML += "End: " + myVar + "<br>";
 
 function start(){
-  welcome();
-  displayProduct("Chocolate bar", 4);
-  displayProduct("Chips", 3);
-  displayProduct("Drink", "2.50");
-    getUserName();
-    getUserAge();
-    getUserMoney();
+    console.log("Caution Button was clicked!");
+    OUTPUT.innerHTML += "<p>Caution Button was clicked!</p>";
 }
 
+function getFormInput(){
+    const NAME_FIELD = document.getElementById("nameField");
+    const AGE_FIELD = document.getElementById("age");
+    const MONEY_FIELD = document.getElementById("moneyField");
+    
+    OUTPUT.innerHTML += "<p>Welcome to my page, " + name + "</p>";
+    OUTPUT.innerHTML += "<p>You are " + age + " years old.</p>";
+}   OUTPUT.innerHTML += "<p>You have $" +$$$ + " in your bank account.</p>";
