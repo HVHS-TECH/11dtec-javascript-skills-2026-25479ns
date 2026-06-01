b89138752fe606aeafb0bc11c030953667c29b5d
25479ns <25479ns@hvhs.school.nz>
2026-06-01 00:29:11 +0000

    Jun 1, 2026, 12:29 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -1,1 +1,132 @@
-/* Paste your code from the last task */
+/****************************
+This is a block comment.
+Put your header comment here!
+****************************/
+
+console.log("Running t15 for_loop.js")
+console.log("Working with for loops in Javascript")
+
+// Variables
+
+let name = "Sandro";
+console.log(name);
+
+let age = "15";
+console.log(age);
+
+let $$$ = 5;
+console.log($$$);
+
+let hobbies = ["Coding", "Reading", "Cooking"];
+console.log(hobbies);
+
+let clothes = ["T-shirt", "Jeans", "Hoddie", "Shoes"];
+console.log(clothes);
+
+let isFlooded = true;
+console.log(isFlooded);
+
+var number1 = 8;
+var number2 = 4;
+var answer;
+
+answer = number1 + number2;
+console.log("add: " + answer);
+answer = number1 - number2;
+console.log("subtract: " + answer);
+answer = number1 * number2;
+console.log("multiply: " + answer);
+answer = number1 / number2;
+console.log("divide: " + answer);
+
+/***************************
+Main Code
+****************************/
+const OUTPUT = document.getElementById("OUTPUT");
+OUTPUT.innerHTML += "<p>Added by JavaScript</p>";
+OUTPUT.innerHTML += "<p>Hello, Mr. Sandro!</p>";
+OUTPUT.innerHTML += "<p>Today is a good day to learn JavaScript</p>";
+OUTPUT.innerHTML += "<p>Best of luck with your 1.2 English assessment but don't worry, you've got this</p>";
+
+/***************************
+Functions
+****************************/
+
+var myVar = 0;
+OUTPUT.innerHTML += "Start: " + myVar + "<br>";
+
+functionOne();
+function functionOne(){
+myVar = 1;
+OUTPUT.innerHTML += "functionOne: " + myVar + "<br>";
+}
+
+functionTwo();
+function functionTwo(){
+myVar = 2;
+OUTPUT.innerHTML += "Function Two: " + myVar + "<br>";
+}
+
+OUTPUT.innerHTML += "End: " + myVar + "<br>";
+
+function start(){
+    console.log("Caution Button was clicked!");
+    OUTPUT.innerHTML += "<p>Caution Button was clicked!</p>";
+}
+
+function getFormInput(){
+
+    OUTPUT.innerHTML = "";
+
+    const NAME_FIELD = document.getElementById("NAME_FIELD");
+    const AGE_FIELD = document.getElementById("AGE_FIELD");
+    const MONEY_FIELD = document.getElementById("MONEY_FIELD");
+
+    let userName = NAME_FIELD.value;
+    let userAge = AGE_FIELD.value;
+    let userMoney = parseFloat(MONEY_FIELD.value);
+
+    OUTPUT.innerHTML += "<h2>Welcome to my page, " + userName + "!</h2>";
+    OUTPUT.innerHTML += "<p>You are " + userAge + " years old.</p>";
+    OUTPUT.innerHTML += "<p>You have $" + userMoney + " in your bank account.</p>";
+
+    if (userMoney >= 5){
+
+        OUTPUT.innerHTML += "<p>A chocolate bar costs $5, so you can afford one.</p>";
+
+        calculateChange("Chocolate Bar", 5);
+        calculateChange("Chips", 4);
+        calculateChange("Drink", 2.50);
+
+    } else {
+
+        OUTPUT.innerHTML += "<p>A chocolate bar costs $5, You cannot even afford a chocolate bar. Get Out!</p>";
+    }
+
+
+function calculateChange(itemName, itemPrice){
+
+    const MONEY_FIELD = document.getElementById("MONEY_FIELD");
+
+    let userMoney = parseFloat(MONEY_FIELD.value);
+
+    let change = userMoney - itemPrice;
+
+    OUTPUT.innerHTML +=
+    "<p>After buying " + itemName +
+    ", you would have $" +
+    change.toFixed(2) + " left.</p>";
+}
+
+cost = CHOOSE_FIELD = document.getElementById("CHOOSE_FIELD");
+
+let chocolateArray = ["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!"];
+let choice = parseInt(CHOOSE_FIELD.value);
+
+if (choice >= 0 && choice <= 3){
+    OUTPUT.innerHTML += "<p>" + chocolateArray[choice] + "</p>";
+} else {
+    OUTPUT.innerHTML += "<p>Invalid choice. Please enter a number between 0 and 3.</p>";
+}
+return userMoney;
+}
